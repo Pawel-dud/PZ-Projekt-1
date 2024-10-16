648467bc765b5ee7464d7c351acab92b7e67e36c
Pawel-dud <pawel_dudziak@spoko.pl>
2024-10-16 22:33:57 +0200

    konstuktor destruktor

--- lista.cpp
+++ lista.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 
+struct elementy
+{
+    elementy * następny, *poprzedni;
+    int klucz;
+};
+
+class lista
+{
+    private:
+    elementy * poczatek, *koniec;
+    unsigned licznik;
+
+    public:
+    lista()
+    {
+        poczatek = NULL;
+        koniec = NULL;
+        licznik = 0;
+    }
+
+    ~lista()
+    {
+
+    }
+};
+
 int main() {
-    
+ cout<<"Hello";
+
 }
