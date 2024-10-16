d992a5f233a67f8cc0d125925767e0e1e7cede2c
Pawel-dud <pawel_dudziak@spoko.pl>
2024-10-16 23:44:36 +0200

    elementy pod wskazany adres

--- lista.cpp
+++ lista.cpp
@@ -26,7 +26,8 @@ class metody
         czyszczenie();
     }
 
-     void dodaj_na_poczatek(int id) {
+    void dodaj_na_poczatek(int id) 
+    {
         elementy *nowy = new elementy;
         nowy->id = id;
         nowy->nastepny = poczatek;
@@ -41,8 +42,63 @@ class metody
             koniec = poczatek;
         }
         licznik++;
+    }  
+
+    void dodaj_na_koniec(int id) 
+    {
+        elementy *nowy = new elementy;
+        nowy->id = id;
+        nowy->nastepny = NULL;
+        nowy->poprzedni = koniec;
+
+        if (koniec) {
+            koniec->nastepny = nowy;
+        }
+        koniec = nowy;
+
+        if (!poczatek) {
+            poczatek = koniec;
+        }
+        licznik++;
     }
 
+
+    void dodaj_pod_indeks(int id, unsigned indeks) 
+    {
+        if (indeks > licznik) 
+        {
+            cout << "Indeks poza zakresem!" << endl;
+            return;
+        }
+        if (indeks == 0) 
+        {
+            dodaj_na_poczatek(id);
+        } else if (indeks == licznik) 
+        {
+            dodaj_na_koniec(id);
+        } else 
+        {
+            elementy *nowy = new elementy;
+            nowy->id = id;
+            elementy *p = poczatek;
+
+            for (unsigned i = 0; i < indeks - 1; i++) 
+            {
+                p = p->nastepny;
+            }
+            nowy->nastepny = p->nastepny;
+            nowy->poprzedni = p;
+            p->nastepny = nowy;
+            if (nowy->nastepny) 
+            {
+                nowy->nastepny->poprzedni = nowy;
+            } else 
+            {
+                koniec = nowy;
+            }
+            licznik++;
+        }
+    }
 };
 
 
